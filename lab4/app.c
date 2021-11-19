4182b6d3c0f467438515c9e52746cbece1db668a
BornikReal <cool.nikolay@gmail.com>
2021-11-19 14:57:41 +0300

    Add getting frame information

--- lab4/app.c
+++ lab4/app.c
@@ -16,11 +16,16 @@ void print_str(char *str_to_print, int size) {
     printf("\n");
 }
 
+void show_frame(frame* cur_frame) {
+    printf("Frame ID: %s\nFrame content: ", cur_frame -> frame_id);
+    print_str(cur_frame -> frame_cont, cur_frame -> frame_size);
+}
+
 int main(int argc, char* argv[]) {
     FILE* musicf;
     musicf = fopen("minecraft.mp3", "r");
     char buf_str[100];
-    int size_of_tag = 0, addit = 128 * 128 * 128;
+    int size_of_tag = 0, addit = 128 * 128 * 128, num = 0;
     for (int i = 0; i < 3; i++)
         buf_str[i] = fgetc(musicf);
     if (!((buf_str[0] == 'I') && (buf_str[1] == 'D') && (buf_str[2] == '3'))) {
@@ -33,25 +38,26 @@ int main(int argc, char* argv[]) {
         addit /= 128;
     }
     printf("%d\n", size_of_tag);
+    frame list_of_frames[100];
     while (ftell(musicf) < (size_of_tag + 10)) {
-        frame new_fr;
         for (int i = 0; i < 4; i++)
-            new_fr.frame_id[i] = fgetc(musicf);
-        if (((new_fr.frame_id[3] < 'A') || (new_fr.frame_id[3] > 'Z')) && ((new_fr.frame_id[3] < '0') || (new_fr.frame_id[3] > '9')))
+            list_of_frames[num].frame_id[i] = fgetc(musicf);
+        if (((list_of_frames[num].frame_id[3] < 'A') || (list_of_frames[num].frame_id[3] > 'Z')) && ((list_of_frames[num].frame_id[3] < '0') || (list_of_frames[num].frame_id[3] > '9')))
             continue;
         addit = 128 * 128 * 128;
-        new_fr.frame_size = 0;
+        list_of_frames[num].frame_size = 0;
         for (int i = 0; i < 4; i++) {
-            new_fr.frame_size += addit * fgetc(musicf);
+            list_of_frames[num].frame_size += addit * fgetc(musicf);
             addit /= 128;
         }
         fseek(musicf, 2, SEEK_CUR);
-        new_fr.frame_cont = malloc(new_fr.frame_size);
-        for (int i = 0; i < new_fr.frame_size; i++)
-            new_fr.frame_cont[i] = fgetc(musicf);
-        new_fr.pos = ftell(musicf);
-        printf("%d %d %s\n", new_fr.frame_size, new_fr.pos, new_fr.frame_id);
-        print_str(new_fr.frame_cont, new_fr.frame_size);
+        list_of_frames[num].frame_cont = malloc(list_of_frames[num].frame_size);
+        for (int i = 0; i < list_of_frames[num].frame_size; i++)
+            list_of_frames[num].frame_cont[i] = fgetc(musicf);
+        list_of_frames[num].pos = ftell(musicf);
+        num++;
     }
+    for (int i = 0; i < num; i++)
+        show_frame(&list_of_frames[i]);
     return 0;
 }
