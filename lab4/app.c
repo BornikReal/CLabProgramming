bf0d677fe7773e670ea1d3ec9dbcc3b9409bd554
BornikReal <cool.nikolay@gmail.com>
2021-12-04 00:40:23 +0300

    Add writing in file and finish the word

--- lab4/app.c
+++ lab4/app.c
@@ -3,17 +3,16 @@
 #include <stdbool.h>
 #include <string.h>
 #include <locale.h>
-// #include <unistd.h>
 
 typedef struct {
     char frame_id[5];
-    int frame_flag[2];
     int frame_size;
     char *frame_cont;
     int pos;
 } frame;
 frame list_of_frames[100];
 int num = 0;
+int size_of_tag = 0;
 
 void print_str(char *str_to_print, int size) {
     for (int i = 0; i < size; i++)
@@ -42,15 +41,17 @@ int find_frame(char* frame_id) {
     return -1;
 }
 
-void change_frame(char* frame_id, char* frame_new_cont) {
+int change_frame(char* frame_id, char* frame_new_cont) {
     int fpos = find_frame(frame_id);
     if (fpos != -1) {
-        int dif = strlen(frame_new_cont) - list_of_frames[fpos].frame_size + 1;
-        list_of_frames[fpos].frame_size = strlen(frame_new_cont) + 1;
+        int dif = strlen(frame_new_cont) - list_of_frames[fpos].frame_size;
+        list_of_frames[fpos].frame_size = strlen(frame_new_cont);
         strcpy(list_of_frames[fpos].frame_cont, frame_new_cont);
         for (int i = (fpos + 1); i < num; i++)
             list_of_frames[i].pos += dif;
+        return dif;
     }
+    return -1;
 }
 
 int main(int argc, char* argv[]) {
@@ -70,7 +71,7 @@ int main(int argc, char* argv[]) {
         return 1;
     }
     if (strstr(argv[1], "--filepath=") && strcmp(argv[1], "--filepath=")) {
-        filepath = malloc(sizeof(char) * (strlen(argv[1]) - 10));
+        filepath = malloc(sizeof(char) * (strlen(argv[1]) - 9));
         strcpy(filepath, argv[1] + 11);
         mode = 0;
     }
@@ -117,7 +118,6 @@ int main(int argc, char* argv[]) {
             return 1;
         }
     }
-    // printf("%s\n", getenv("TEMP"));
     FILE* musicf;
     musicf = fopen(filepath, "rb");
     if (!musicf) {
@@ -125,7 +125,7 @@ int main(int argc, char* argv[]) {
         return 1;
     }
     char buf_str[100];
-    int size_of_tag = 0, addit = 128 * 128 * 128;
+    int addit = 128 * 128 * 128;
     for (int i = 0; i < 3; i++)
         buf_str[i] = fgetc(musicf);
     if (!((buf_str[0] == 'I') && (buf_str[1] == 'D') && (buf_str[2] == '3'))) {
@@ -143,14 +143,14 @@ int main(int argc, char* argv[]) {
         list_of_frames[num].frame_id[4] = '\0';
         if (((list_of_frames[num].frame_id[3] < 'A') || (list_of_frames[num].frame_id[3] > 'Z')) && ((list_of_frames[num].frame_id[3] < '0') || (list_of_frames[num].frame_id[3] > '9')) && ((list_of_frames[num].frame_id[3] < 'А') || (list_of_frames[num].frame_id[3] > 'Я')))
             continue;
+        list_of_frames[num].pos = ftell(musicf) - 4;
         addit = 128 * 128 * 128;
         list_of_frames[num].frame_size = 0;
         for (int i = 0; i < 4; i++) {
             list_of_frames[num].frame_size += addit * fgetc(musicf);
             addit /= 128;
         }
-        list_of_frames[num].frame_flag[0] = fgetc(musicf);
-        list_of_frames[num].frame_flag[1] = fgetc(musicf);
+        fseek(musicf, 2, SEEK_CUR);
         if (list_of_frames[num].frame_id[0] != 'T') {
             fseek(musicf, list_of_frames[num].frame_size, SEEK_CUR);
             continue;
@@ -158,7 +158,6 @@ int main(int argc, char* argv[]) {
         list_of_frames[num].frame_cont = malloc(list_of_frames[num].frame_size);
         for (int i = 0; i < list_of_frames[num].frame_size; i++)
             list_of_frames[num].frame_cont[i] = fgetc(musicf);
-        list_of_frames[num].pos = ftell(musicf);
         num++;
     }
     if (mode == 0)
@@ -178,14 +177,55 @@ int main(int argc, char* argv[]) {
         }
     }
     else if (mode == 2) {
-        if (find_frame(input1) == -1) {
+        int pos1 = find_frame(input1);
+        if (pos1 == -1) {
             printf("Frame not found");
             return 1;
         }
-        change_frame(input1, input2);
+        int dif = change_frame(input1, input2);
         FILE* musico;
-        musico = fopen(filepath, "wb");
-        // show_frame(&list_of_frames[find_frame(input1)]);
+        char *temp_path = malloc(strlen(getenv("TEMP")) + strlen("\\temp.mp3") + 1);
+        strcpy(temp_path, getenv("TEMP"));
+        strcat(temp_path, "\\temp.mp3");
+        musico = fopen(temp_path, "wb");
+        fseek(musicf, 3, SEEK_SET);
+        fprintf(musico, "ID3");
+        for (int i = 0; i < 3; i++)
+            fputc(fgetc(musicf), musico);
+        char temp[4];
+        int temp1 = size_of_tag + dif + 1;
+        for (int i = 0; i < 4; i++) {
+            temp[i] = temp1 % 128;
+            temp1 /= 128;
+        }
+        for (int i = 3; i >= 0; i--)
+            fputc(temp[i], musico);
+        fseek(musicf, 4, SEEK_CUR);
+        for (int i = 10; i < list_of_frames[pos1].pos + 4; i++)
+            fputc(fgetc(musicf), musico);
+        temp1 = list_of_frames[pos1].frame_size + 1;
+        for (int i = 0; i < 4; i++) {
+            temp[i] = temp1 % 128;
+            temp1 /= 128;
+        }
+        for (int i = 3; i >= 0; i--)
+            fputc(temp[i], musico);
+        fputc(fgetc(musicf), musico);
+        fputc(fgetc(musicf), musico);
+        fputc(0, musico);
+        for (int i = 0; i < strlen(list_of_frames[pos1].frame_cont); i++)
+            fputc(list_of_frames[pos1].frame_cont[i], musico);
+        fseek(musicf, list_of_frames[pos1].frame_size - dif + 4, SEEK_CUR);
+        for (int i = ftell(musicf); i < size_of_tag; i++)
+            fputc(fgetc(musicf), musico);
+        char c;
+        while (fscanf(musicf, "%c", &c) != EOF) {
+            fprintf(musico, "%c", c);
+        }
+        fclose(musico);
+        fclose(musicf);
+        remove(filepath);
+        rename(temp_path, filepath);
     }
     return 0;
 }
