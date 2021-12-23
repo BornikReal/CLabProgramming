05560c730a1817732e236930e804076943806c4d
BornikReal <cool.nikolay@gmail.com>
2021-12-23 14:41:46 +0300

    Add tests

--- MultiMap/main.cpp
+++ MultiMap/main.cpp
@@ -217,31 +217,31 @@ public:
     }
 };
 
-int main()
-{
-   ios_base::sync_with_stdio(false);
-   cin.tie(NULL);
-   freopen("multimap.in", "r", stdin);
-   freopen("multimap1.out", "w", stdout);
-   MultiMap my_multi_map;
-   string input, input1, input2;
-   while (cin >> input)
-   {
-       cin >> input1;
-       if (input == "put")
-       {
-           cin >> input2;
-           my_multi_map.multi_map_put(input1, input2);
-       }
-       else if (input == "delete")
-       {
-           cin >> input2;
-           my_multi_map.multi_map_delete(input1, input2);
-       }
-       else if (input == "deleteall")
-           my_multi_map.multi_map_deleteall(input1);
-       else if (input == "get")
-           my_multi_map.multi_map_get(input1);
-   }
-   return 0;
-}
+// int main()
+// {
+//    ios_base::sync_with_stdio(false);
+//    cin.tie(NULL);
+//    freopen("multimap.in", "r", stdin);
+//    freopen("multimap1.out", "w", stdout);
+//    MultiMap my_multi_map;
+//    string input, input1, input2;
+//    while (cin >> input)
+//    {
+//        cin >> input1;
+//        if (input == "put")
+//        {
+//            cin >> input2;
+//            my_multi_map.multi_map_put(input1, input2);
+//        }
+//        else if (input == "delete")
+//        {
+//            cin >> input2;
+//            my_multi_map.multi_map_delete(input1, input2);
+//        }
+//        else if (input == "deleteall")
+//            my_multi_map.multi_map_deleteall(input1);
+//        else if (input == "get")
+//            my_multi_map.multi_map_get(input1);
+//    }
+//    return 0;
+// }
